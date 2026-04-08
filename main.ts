9efb8124946f144164a62576faee4a8a682ab401
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-04-08 12:23:29 -0400

    changed code

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
-/* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+/*
+* Copyright (c) 2020 MTHS All rights reserved
+*
+* Created by: Kyle Matthew
+* Created on: Apr 2026
+* This program uses the distance sensor and saves the current distance in cm to a variable.
 */
 
-basic.showString('Hello, World!')
+// variables
+let distance = 0
+
+// setup
+let strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+
+input.onButtonPressed(Button.A, function () {
+    distance = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    if (distance < 10) {
+        strip.showColor(NeoPixelColors.Red)
+    } else {
+        strip.showColor(NeoPixelColors.Green)
+    }
+})
